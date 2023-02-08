a36dff1017b88a0cac1ff76a63fde5b80fe84b97
solomon <solomonf1227@gmail.com>
2023-02-08 13:29:12 +0300

    Update models/base_model.py: to link your BaseModel to FileStorage by using the variable storage

--- models/base_model.py
+++ models/base_model.py
@@ -28,6 +28,8 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.now()
             self.updated_at = datetime.datetime.now()
+            FileStorage.storage.new()
+
     def __str__(self):
         """Prinnt string representation of an object"""
         return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
@@ -35,6 +37,7 @@ class BaseModel:
     def save(self):
         """updates the public instance attribute updated_at with the current datetime"""
         self.updated_at = datetime.datetime.now()
+        FileStorage.storage.save(self)
 
     def to_dict(self):
         """ returns a dictionary containing all keys/values of
